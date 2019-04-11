d68ae314e13c4ba912450687951675b11bc8822a
Yernar <yernar.mussin@nitka.com>
2019-04-11 23:40:59 +0600

    added service to getHeroes()

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
 
 @Component({
@@ -9,16 +9,20 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
   selectedHero: Hero;
-  heroes = HEROES;
+  heroes: Hero[];
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  constructor(private heroService: HeroService) {}
 
   ngOnInit() {
-
+    this.getHeroes();
   }
 
 }
