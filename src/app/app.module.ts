a8c777c1c498d83fa7fb28ad2127bed7dab5a077
Yernar <yernar.mussin@nitka.com>
2019-04-12 15:54:05 +0600

    added auth component/service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -11,6 +11,7 @@ import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroIdComponent } from './hero-id/hero-id.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { HeroIdComponent } from './hero-id/hero-id.component';
     HeroDetailComponent,
     MessagesComponent,
     DashboardComponent,
-    HeroIdComponent
+    HeroIdComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
